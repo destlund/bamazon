5dbad116bfd2ff4bb738f00b28aa874a4f93b609
David Estlund <destlund@gmail.com>
2018-01-30 15:32:39 -0600

    Fast and dirty, but it works! Time to clean up the console.log entries and put it to use.

---
+++ bamazon-customer.js
@@ -0,0 +1,95 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    port: 8889,
+    database: 'bamazon'
+});
+
+connection.connect(function (err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
+    shopNow();
+});
+
+var shopNow = function () {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'item_id',
+            message: "What is the ID of the items you wish to purchase?"
+        },
+        {
+            type: 'input',
+            name: 'number',
+            message: "How many would you like?",
+            default: function () {
+                return 1;
+            }
+        }
+    ]).then(function (answers) {
+        // this is where we hit the database for stuff
+        // let's find our item
+        connection.query('SELECT * FROM products WHERE  item_id = ?', answers.item_id, function (error, results, fields) {
+            if (error) console.log('You broke it! ' + error);
+            var string = JSON.stringify(results)
+            console.log(string);
+            var itemObject = JSON.parse(string);
+            console.log(itemObject[0]);
+
+            // check whether it's a valid item
+            if (itemObject[0] == undefined) {
+                console.log("That product does not exist!");
+                shopAgain();
+                return;
+            };
+
+            // now let's check to see if there are enough of the item to fill the order
+            if (results[0].stock_quantity < answers.number) {
+                console.log("There are not " + answers.number + " " + itemObject[0].product_name + "s in stock right now. Check back later.")
+                shopAgain();
+                return;
+            };
+            var placeOrderSQL = 'UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?'
+            connection.query(placeOrderSQL, [parseInt(answers.number), parseInt(answers.item_id)], function(err, result) {
+                if (err) {
+                    console.log('CRITICAL FAILURE! ABORT! ABORT! ' + err);
+                    return;
+                };
+                console.log("Success! They're on their way.\nThere are " + (results[0].stock_quantity - answers.number) + " " + results[0].product_name + "s remaining in stock.")
+                shopAgain();
+                return;
+            })
+
+            // connection.query('');
+            // now let's remove that number of items from inventory (and ship them of course)
+            // shall we continue shopping?
+            return;
+        });
+
+    });
+};
+
+// prompt the user to see if they're finished
+var shopAgain = function () {
+    inquirer.prompt({
+        type: 'confirm',
+        name: 'shopAgain',
+        message: "Would you like to make another purchase?",
+        default: false
+    }).then(function (response) {
+        console.log(response.shopAgain);
+        if (response.shopAgain === false) {
+            console.log('Your products are on their way! Goodbye.');
+            connection.end();
+            return;
+        }
+        shopNow(response);
+    })
+}
--- bamazon.js
+++ bamazon.js
@@ -14,17 +14,6 @@ connection.connect(function (err) {
     }
     console.log('connected as id ' + connection.threadId);
 
-    // fs.readFile(__dirname + "/make-bamazon.sql", "ascii", function(err, data) {
-    //     if (err) throw err;
-    //     console.log(data);
-    //     connection.query(data, function(err, res, fields) {
-    //         if (err) throw err;
-    //         console.log(res);
-    //         console.log('Bamazon Database Initialized')
-    //     });
-    // });
-//     // connection.query(fs.readSync(__dirname + "/make-bamazon.sql"), function (error, results, fields) {
-//         // if (error) throw error;
-//         console.log('Bamazon Database Initialized');
-//     // });
+    
+
 });
